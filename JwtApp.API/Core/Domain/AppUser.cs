e2afb5402f8bced3f6f4cd79c71dba953a24bdc5
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-20 05:13:15 +0300

    Gerekli ortamlar kuruldu: db, entityler ve migration

---
+++ JwtApp.API/Core/Domain/AppUser.cs
@@ -0,0 +1,20 @@
+﻿namespace JwtApp.API.Core.Domain
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+
+        public int AppRoleId { get; set; }
+
+        public AppRole AppRole { get; set; }
+
+        public AppUser()
+        {
+            AppRole = new AppRole();
+        }
+    }
+}
---
+++ JwtApp.API/Core/Domain/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace JwtApp.API.Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string? Definition { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
