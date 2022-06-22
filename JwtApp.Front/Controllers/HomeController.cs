8364516b7579d6987c4bf47ecf7819b68080a038
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-22 16:36:18 +0300

    Front projesi eklendi.

---
+++ JwtApp.Front/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JwtApp.Front.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
